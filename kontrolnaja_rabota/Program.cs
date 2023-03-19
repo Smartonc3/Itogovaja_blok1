0a454a988bf6b04e20edb98b4538a9b685e71f79
smartonc3 <smartonc3@inbox.lv>
2023-03-19 17:07:57 +0000

    Добавили выполненую задачу

--- kontrolnaja_rabota/Program.cs
+++ kontrolnaja_rabota/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿string[] array = new string[6] { "3", "3421", "334", "4d", "rdffgg", "52" };
+string[] array2 = new string[array.Length];
+
+void SecondArrayWithIF(string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[count] = array[i];
+            count++;
+        }
+    }
+}
+
+
+void PrintArray(string[] print)
+{
+    for (int i = 0; i < print.Length; i++)
+    {
+        Console.Write($"{print[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+SecondArrayWithIF(array, array2);
+PrintArray(array2);
